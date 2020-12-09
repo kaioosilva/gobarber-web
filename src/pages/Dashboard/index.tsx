327be11f9488c04d2977be8c2a8d2b36be6c40c7
Kaio <kaioo_silva@outlook.com>
2020-12-09 15:39:04 +0000

    List of appointments and calendar created

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,15 +1,33 @@
-import React from 'react';
-import { FiPower } from 'react-icons/fi';
+import React, { useCallback, useState } from 'react';
+import DayPicker, { DayModifiers } from 'react-day-picker';
+import 'react-day-picker/lib/style.css';
+
+import { FiClock, FiPower } from 'react-icons/fi';
+import logoImg from '../../assets/logo.svg';
 import { useAuth } from '../../hooks/Auth';
 
-import logoImg from '../../assets/logo.svg';
-
-import { Container, Header, HeaderContent, Profile } from './styles';
-import userEvent from '@testing-library/user-event';
+import { 
+    Container, 
+    Header, 
+    HeaderContent, 
+    Profile, 
+    Content, 
+    Schedule, 
+    NextAppointment, 
+    Section,
+    Appointment,
+    Calendar } 
+from './styles';
 
 const Dashboard: React.FC = () => {
-
+    const [selectedDate, setSelectedDate] = useState(new Date());
     const { signOut, user } = useAuth();
+
+    const handleDateChange = useCallback((day: Date, modifiers: DayModifiers) => {
+        if(modifiers.available) {
+            setSelectedDate(day);
+        }
+    }, []);
 
     return (
         <Container>
@@ -33,6 +51,100 @@ const Dashboard: React.FC = () => {
                     </button>
                 </HeaderContent>
             </Header>
+
+            <Content>
+                <Schedule>
+                    <h1>Appointments</h1>
+                    <p>
+                        <span>Today</span>
+                        <span>Day 06</span>
+                        <span>Monday</span>
+                    </p>
+
+                    <NextAppointment>
+                       <strong>Next appointment</strong> 
+                       <div>
+                            <img 
+                                src="https://avatars1.githubusercontent.com/u/60159866?s=460&u=a969fab6599ad3a034deac2927fbb76959e8cd14&v=4" 
+                                alt="Kaio Silva"
+                            />
+
+                            <strong>Kaio Silva</strong>
+                            <span>
+                                <FiClock />
+                                08:00
+                            </span>
+                       </div>
+                    </NextAppointment>
+
+                    <Section>
+                        <strong>Morning</strong>
+
+                        <Appointment>
+                            <span>
+                                <FiClock />
+                                08:00
+                            </span>
+
+                            <div>
+                                <img 
+                                    src="https://avatars1.githubusercontent.com/u/60159866?s=460&u=a969fab6599ad3a034deac2927fbb76959e8cd14&v=4" 
+                                    alt="Kaio Silva"
+                                />
+
+                                <strong>Kaio Silva</strong>
+                            </div>
+                        </Appointment>
+
+                        <Appointment>
+                            <span>
+                                <FiClock />
+                                08:00
+                            </span>
+
+                            <div>
+                                <img 
+                                    src="https://avatars1.githubusercontent.com/u/60159866?s=460&u=a969fab6599ad3a034deac2927fbb76959e8cd14&v=4" 
+                                    alt="Kaio Silva"
+                                />
+
+                                <strong>Kaio Silva</strong>
+                            </div>
+                        </Appointment>
+                    </Section>
+
+                    <Section>
+                        <strong>Afternoon</strong>
+
+                        <Appointment>
+                            <span>
+                                <FiClock />
+                                08:00
+                            </span>
+
+                            <div>
+                                <img 
+                                    src="https://avatars1.githubusercontent.com/u/60159866?s=460&u=a969fab6599ad3a034deac2927fbb76959e8cd14&v=4" 
+                                    alt="Kaio Silva"
+                                />
+
+                                <strong>Kaio Silva</strong>
+                            </div>
+                        </Appointment>
+                    </Section>
+                </Schedule>
+                <Calendar>
+                    <DayPicker 
+                        fromMonth={new Date()}
+                        disabledDays={[{ daysOfWeek: [0,6] }]}
+                        modifiers={{
+                            available: { daysOfWeek: [1, 2, 3, 4, 5] },
+                        }}
+                        selectedDays={selectedDate}
+                        onDayClick={handleDateChange}
+                    />
+                </Calendar>
+            </Content>
         </Container>
     );
 };
